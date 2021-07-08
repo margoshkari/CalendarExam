422fe68f45f75f80343ec16d08b075f7d4f454c0
Margo <margoshka.ri6@gmail.com>
2021-07-08 19:49:05 +0300

    Added GUI to NewNoteForm

---
+++ NewNoteForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace CalendarExam
+{
+    partial class NewNoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(12, 12);
+            this.titleTextBox.Multiline = true;
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(294, 45);
+            this.titleTextBox.TabIndex = 0;
+            this.titleTextBox.Text = "Title";
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Location = new System.Drawing.Point(12, 77);
+            this.noteTextBox.Multiline = true;
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.noteTextBox.Size = new System.Drawing.Size(294, 326);
+            this.noteTextBox.TabIndex = 1;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(12, 416);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(294, 22);
+            this.dateTimePicker.TabIndex = 2;
+            // 
+            // NewNoteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 450);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.noteTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Name = "NewNoteForm";
+            this.Text = "NewNoteForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox noteTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+    }
+}
