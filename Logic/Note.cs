95474b4fe3f63ae19696cd6821ede470993aae64
Margo <margoshka.ri6@gmail.com>
2021-07-08 20:03:01 +0300

    Released class Note

---
+++ Logic/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Note
+    {
+        public string NoteName { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        public Note()
+        {
+            NoteName = string.Empty;
+            Text = string.Empty;
+            Date = new DateTime();
+        }
+
+        public Note(string noteName, string text, DateTime creationData)
+        {
+            Text = text;
+            NoteName = noteName;
+            Date = creationData;
+        }
+    }
+}
